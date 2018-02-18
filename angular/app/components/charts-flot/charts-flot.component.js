65f9476fdf2d9d71ff8d56c5c6115ee80733367f
Mark Clu Heramis <chumheramis@gmail.com>
2018-02-18 17:47:43 +0800

    flot charts page created (not yet done, require flot-angular?)
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ angular/app/components/charts-flot/charts-flot.component.js
@@ -0,0 +1,17 @@
+class ChartsFlotController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const ChartsFlotComponent = {
+    templateUrl: './views/app/components/charts-flot/charts-flot.component.html',
+    controller: ChartsFlotController,
+    controllerAs: 'vm',
+    bindings: {}
+}
