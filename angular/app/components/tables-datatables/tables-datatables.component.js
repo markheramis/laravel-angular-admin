52da7db1814f1b08afa8ada7e707f2107eee0646
Mark Clu Heramis <chumheramis@gmail.com>
2018-02-18 17:48:57 +0800

    tables datatables page created
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ angular/app/components/tables-datatables/tables-datatables.component.js
@@ -0,0 +1,17 @@
+class TablesDatatablesController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const TablesDatatablesComponent = {
+    templateUrl: './views/app/components/tables-datatables/tables-datatables.component.html',
+    controller: TablesDatatablesController,
+    controllerAs: 'vm',
+    bindings: {}
+}
---
+++ tests/angular/app/components/tables-datatables.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test tables-datatables component',
+    modules: 'app',
+    element: '<tables-datatables></tables-datatables>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
