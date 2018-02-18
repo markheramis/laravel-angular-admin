4ce0c2e844a040223ecd6729fb099a784ab3b1fc
Mark Clu Heramis <chumheramis@gmail.com>
2018-02-18 17:46:59 +0800

    blank page component added
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ tests/angular/app/components/blank.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test blank component',
+    modules: 'app',
+    element: '<blank></blank>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
